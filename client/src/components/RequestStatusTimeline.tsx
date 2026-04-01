df6680a4d060e56f0357ab68f665ee324ac07cbb
malikamaan12 <amaanmalik12@gmail.com>
2026-04-02 00:11:00 +0300

    Exclude and remove legacy client directory to resolve Vercel build errors

--- client/src/components/RequestStatusTimeline.tsx
+++
@@ -1,5 +0,0 @@
-// This component has been deprecated in favor of RequestTimeline
-// File kept for reference but should not be used
-export default function RequestStatusTimeline() {
-  return null;
-}
